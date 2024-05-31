3fdab828219f28aa7cfe26652d072db0621c8994
Snazrimuh~ <snazrimuh@gmail.com>
2024-05-31 09:14:29 +0700

    kpu logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,7 +6,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="#" target="_blank">
-            <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="../assets/img/kpu-uns.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Pemira Dashboard</span>
         </a>
     </div>
@@ -44,8 +44,8 @@
                 </a>
             </li>
             <li class="nav-item pb-2">
-                <a class="nav-link {{ (Request::is('admin.manage-election') ? 'active' : '') }}" 
-                href="{{ route('admin.manage_election') }}">
+                <a class="nav-link {{ (Request::is('admin.manage-election') ? 'active' : '') }}"
+                    href="{{ route('admin.manage_election') }}">
                     <span class="nav-link-text ms-1">Election Management</span>
                 </a>
             </li>
