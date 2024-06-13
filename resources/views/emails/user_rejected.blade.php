71af8a770486d749d506786360b7e0a14d405289
Snazrimuh~ <snazrimuh@gmail.com>
2024-06-13 19:38:41 +0700

    email smtp

--- resources/views/emails/user_rejected.blade.php
+++ resources/views/emails/user_rejected.blade.php
@@ -6,10 +6,10 @@
 </head>
 
 <body>
-    <h1>Your Account Has Been Rejected</h1>
+    <h1>Your Pemira Account Has Been Rejected</h1>
     <p>Dear {{ $user->name }},</p>
-    <p>We regret to inform you that your account has been rejected. If you have any questions, please contact our
-        support team.</p>
+    <p>We regret to inform you that your Pemira account registration has been rejected. Please re-register to be able to
+        participate in the election. If you have any questions, please contact our support team.</p>
     <p>Thank you.</p>
 </body>
 
